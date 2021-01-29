7f9c8188fe11efec26e038a36992cd24ae9f2b0e
jester9898 <52189526+jester9898@users.noreply.github.com>
2021-01-29 16:58:27 -0500

    Training by Michael

--- src/main.py
+++ src/main.py
@@ -1,1 +1,50 @@
-print("hello world")
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+"""
+1. Using X_train, Y_train to train model. 
+
+2. Then using X_test, Y_test to predict new dataset called Y_predict
+
+3. Compare Y_predict with Y_test
+
+If Y_test is statistically significant, your model is accurate. 
+
+
+
+Using two models, RandomForest & ExtremeTree
+Which ever has lower Y_test is better.
+"""
+
+
+
+
+train_data = pd.read_csv("../data/titanic/train.csv")
+test_data = pd.read_csv("../data/titanic/test.csv")
+#print(train_data.head())
+
+
+
+woman = train_data.loc[train_data.Sex == "female"][['Survived']]
+
+# print(len(woman), sum(woman.Survived), type(woman))
+ws_rate = sum(woman.Survived) / len(woman)
+
+print(f'% women survived {ws_rate} - which is good')
+
+
+
+
+ax = sns.countplot(x = "Sex", hue = "Survived", data = train_data)
+#plt.show()
+
+
+
+# dt = DecisionTree()
+# dt.train()
+# dt.pred(test)
---
+++ src/training.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Using python train_test_split to split dataset into training and testing dataset.
+
+dataset = pd.read_csv("../data/titanic/train.csv")
+productionDataset = pd.read_csv("../data/titanic/test.csv")
+
+dataset_train = dataset[["Pclass", "Sex", "SibSp", "Parch"]]
+dataset_test = dataset[["Pclass", "Sex", "SibSp", "Parch"]]
+# Separate dataset to categories
+
+dataset_train_arr = dataset_train.to_numpy()
+dataset_test_arr = dataset_test.to_numpy()
+X_train, X_test, Y_train, Y_test = train_test_split(dataset_train_arr, dataset_test_arr, test_size=0.33, random_state=30)
